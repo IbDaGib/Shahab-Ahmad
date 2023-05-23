f41789670ae765d869f85973c7b608c51657478a
Muhammad Ibrahim <IbDaGib@gmail.com>
2023-05-23 08:07:43 -0400

    Pushing Initial Draft
    Fixes:
    
    NAV menu needs to be implemented on smaller screens
    
    Text on Nav needs to be able to be seen over images
    
    Images need a better spacing and layout
    
    images need to be functional serve as links to galleries
    
    Poetry section still under construction
    
    About section still under construction
    
    Backend needs to be implemented. Things like Owner Login. Making posts and deleting posts. Linking google photo galleries. reordering images.

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ References used to make this site:
 https://glitch.com/edit/#!/google-photos-album-demo2?path=google-photos.js%3A1%3A0
 
 https://tomhull.co.uk/info
+
+https://css-tricks.com/adaptive-photo-layout-with-flexbox/
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Path: scripts/script.js
 // Get the current page URL
 const url = window.location.href;
 
@@ -9,5 +10,19 @@ navLinks.forEach(link => {
   if (link.href === url) {
     // Add the "active" class to the link if it matches the current URL
     link.classList.add('active');
+  }
+});
+
+// When nav-menu clicked on it becomes "Close"
+const navMenu = document.querySelector('.nav-menu');
+
+
+navMenu.addEventListener('click', function() {
+  if (navMenu.textContent === 'Menu') {
+    navMenu.textContent = 'Close';
+    navMenu.classList.add('active');
+  } else {
+    navMenu.textContent = 'Menu';
+    navMenu.classList.remove('active');
   }
 });
